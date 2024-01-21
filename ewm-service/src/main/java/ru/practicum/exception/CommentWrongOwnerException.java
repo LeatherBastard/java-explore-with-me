7e2980734ac7094e963cf755a159847a62ea88b5
LeatherBastard <kostrykinmark@yandex.ru>
2024-01-21 15:14:04 +0300

    style: reformat code at CommentWrongOwnerException

--- ewm-service/src/main/java/ru/practicum/exception/CommentWrongOwnerException.java
+++ ewm-service/src/main/java/ru/practicum/exception/CommentWrongOwnerException.java
@@ -1,6 +1,6 @@
 package ru.practicum.exception;
 
-public class CommentWrongOwnerException extends RuntimeException{
+public class CommentWrongOwnerException extends RuntimeException {
     public CommentWrongOwnerException(int comId, int userId) {
         super(String.format("Comment with id %d has an owner with id %d", comId, userId));
     }
